43fd54de5ddd545a5493d1329519f2291b6dedfe
Vitaliy_B <vlvlv.vovov@bk.ru>
2020-12-11 12:59:15 +0500

    добавил возможность стилизации темы material-ui

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './theme';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+      <ThemeProvider theme={theme} >
+
+      </ThemeProvider>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
---
+++ frontend/src/pages/SignIn.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SignIn () {
+
+    return <p>
+        sign
+    </p>
+}
+
+
+export default SignIn;

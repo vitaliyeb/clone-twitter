60ae8a245c33ae37b5cc4d22b8a8765428810f84
vitaliyeb <vlvlv.vovov@bk.ru>
2020-12-28 19:36:03 +0300

    rrd ts

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from '@material-ui/core/styles';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import theme from './theme';
 import './index.css';
 import App from './App';

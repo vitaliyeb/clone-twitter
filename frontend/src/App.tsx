6eb1869cf41932198a162111c2ab6751e460ffcc
vitaliyeb <vlvlv.vovov@bk.ru>
2020-12-29 09:51:55 +0300

    тестовая маршрутизация

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import SignIn from './pages/SignIn';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './pages/home';
 
 function App() {
   return (
     <div className="App">
-        <SignIn />
+      <Router>
+         <Switch>
+           <Route exact path='/'>
+              <SignIn/>
+           </Route>
+           <Route exact path='/home/'>
+              <Home/>
+           </Route>
+         </Switch>
+      </Router>
     </div>
   );
 }
